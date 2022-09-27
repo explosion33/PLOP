d1d74f131a68922dee007a27a5b4e4cf703aa3fe
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-09-27 01:58:05 -0700

    added gitignore

--- Rust/target/debug/deps/Rust-03a780576e370a74.d
+++
@@ -1,5 +0,0 @@
-c:\Users\wetha\Desktop\School\SARP\PLOP\code\Rust\target\debug\deps\Rust-03a780576e370a74.rmeta: src\main.rs
-
-c:\Users\wetha\Desktop\School\SARP\PLOP\code\Rust\target\debug\deps\Rust-03a780576e370a74.d: src\main.rs
-
-src\main.rs:
