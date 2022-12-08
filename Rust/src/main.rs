af2aa799edc4ecdd9f0d399037505de13090d784
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-12-07 18:11:18 -0800

    updated main code to print data for all sensors

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,25 +1,71 @@
-use crate::igniter::Igniter;
-mod igniter;
+//use crate::igniter::Igniter;
+//mod igniter;
 
 use crate::baro::Baro;
 mod baro;
 
-use crate::indicator::Indicator;
-mod indicator;
+use crate::imu::IMU;
+mod imu;
+
+use crate::gps::{GPS, GpsData};
+mod gps;
+
+//use crate::indicator::Indicator;
+//mod indicator;
 
 fn main() {
-    // INIT SENSORS
-    // INIT LED
 
-    // CHECK IGNITER CONTINUITY
+    let mut imu = IMU::new("imu.conf");
+    let mut baro = Baro::new("bar.conf").unwrap();
+    let mut gps = GPS::new();
+    //imu.calibrate();
 
+    loop {
+        match gps.get_data() {
+            Some(data) => {
+                println!("\n{:?}", data)
+            },
+            _ => {
+                //print!(".");
+                //stdout().flush();
+                //thread::sleep(Duration::from_millis(50));
+            },
+        }
 
-    // GET SENSOR DATA
-    // GET KALMAN ALT
+        let (a,b,c) = match imu.euler() {
+            Some(n) => n,
+            _ => {
+                println!("lost value");
+                (0f32,0f32,0f32)
+            },
+        };
 
-    // LOG
+        let (t0, t1, t2, t3) = match imu.quaternion() {
+            Some(n) => {
+                (n.s, n.v.x, n.v.y, n.v.z)
+            },
+            _ => {
+                println!("lost value");
+                (0f32, 0f32, 0f32, 0f32)
+            }
+        };
+        
+        match imu.accel() {
+            Some((x,y,z)) => {
+                println!("{:8.0}, {:8.0}, {:8.0} | {:8.0}, {:8.0}, {:8.0} | {:8.4}, {:8.4}, {:8.4}, {:8.4}", x.abs(), y.abs(), z.abs(), a, b, c, t0, t1, t2, t3);
+            },
+            _ => {},
+        }
+        
+        match baro.get_alt_variance(10, None) {
+            Ok((alt, var)) => {
+                println!("baro: {alt}, {var}");
+            },
+            Err(_) => todo!(),
+        }
+    }
+    
 
-    // DETECT CRITICAL ALTITUDE
 
     // FIRE IGNITER
 }
