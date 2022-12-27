597d9998559a18d73fa6c09fc2cbdef5196ca735
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2022-12-27 14:49:39 -0800

    added bettor sensor error calculations added rolling average added more comments and TODOs

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,3 +1,13 @@
+// tuning constants
+const ACCEL_ROLLING_AVERAGE: usize = 10;
+const BARO_ROLLING_AVERAGE: usize = 10;
+
+const ERROR_ITER_ACCEL: usize = 1000;
+const ERROR_ITER_BARO:  usize = 200;
+
+const ACCEL_WEIGHT: f32 = 1f32; // 1 is no modification (lower value is higher priority)
+const BARO_WEIGHT: f32 = 1f32;
+
 //use crate::igniter::Igniter;
 //mod igniter;
 
@@ -41,10 +51,8 @@ fn main() {
         * TODO:
         * test effectiveness of performing a rolling average over accel and baro
         *   adjust window size and compare results
-        * better characterize noise of each sensor, perhaps build in weight system
         * extract Z component of GPS velocity (see TODO below)
         * ensure IMU acceleration rotations are working properly (not sure how to do this)
-        * clean-up async code from barometer class (no longer using)
         * look into running multiple i2c lines on raspberry pi to seperate sensors
         * look into creating a seperate Kalman Filter to manage velocity
         *   update velocity based on predicted acceleromter
@@ -53,43 +61,126 @@ fn main() {
         */
         
 
+    // ====================================================================
+    // INIT AND CALIBRATE SENSORS
+    // ====================================================================
     let mut imu = IMU::new("imu.conf");
     let mut baro = Baro::new("baro.conf").unwrap();
     let mut gps = GPS::new();
 
-    baro.configure(189.7f32);
-    //baro.start_async(10);
+    // TODO: configure baro based on GPS altitude?
+    baro.configure(189.7f32, 50);
 
     
     //imu.calibrate();
     imu.calibrate_static_erorr();
 
+    // ====================================================================
+    // CHARACTERIZE SENSOR NOISE
+    // ====================================================================
+    println!("characterizing accel noise");
+    let mut i: usize = ERROR_ITER_ACCEL;
+    let mut vals: [f32; ERROR_ITER_ACCEL] = [0f32; ERROR_ITER_ACCEL];
+    let mut mean: f32 = 0f32;
+    while i > 0 {
+        match imu.accel() {
+            Some((_, _, z)) => {
+                vals[i-1] = z;
+                mean += z;
+                i -= 1;
+            },
+            _ => {},
+        };
+    }
+    mean /= ERROR_ITER_ACCEL as f32;
+
+    let mut stdev: f32 = 0f32;
+
+    for i in 0..ERROR_ITER_ACCEL {
+        stdev += (mean - vals[i]) * (mean - vals[i]);
+    }
+
+    stdev /= ERROR_ITER_ACCEL as f32;
+    let ACCEL_NOISE: f32 = stdev.sqrt();
+
+    
+
+    println!("characterizing baro noise");
+    i = ERROR_ITER_BARO;
+    let mut vals: [f32; ERROR_ITER_BARO] = [0f32; ERROR_ITER_BARO];
+    let mut mean: f32 = 0f32;
+
+    while i > 0 {
+        match baro.get_alt() {
+            Some(alt) => {
+                vals[i-1] = alt;
+                mean += alt;
+                i -= 1;
+            },
+            _ => {
+            },
+        };
+    }
+    
+    mean /= ERROR_ITER_BARO as f32;
+
+    let mut stdev: f32 = 0f32;
+
+    for i in 0..ERROR_ITER_BARO {
+        stdev += (mean - vals[i]) * (mean - vals[i]);
+    }
+
+    stdev /= ERROR_ITER_BARO as f32;
+    let BARO_NOISE: f32 = stdev.sqrt();
+
+    println!("accel noise: {}, baro noise: {}", ACCEL_NOISE, BARO_NOISE);
+
+    // ====================================================================
+    // FILTER INITIALIZATION
+    // ====================================================================
+
+    // dt
     let mut start = Instant::now();
 
+    // tracked values
     let mut vel: f32 = 0f32;
 
-    let mut filter = Filter {
+    let mut pos_filter = Filter {
         alt: 191f32,
         var: 0f32,
-    };
+    }; // Kalman altitude
 
-    let mut accels: [f32; 10] = [0f32; 10];
-    let mut baros: [f32; 10] = [0f32; 10];
+    // rolling averages
+    let mut accels: [f32; ACCEL_ROLLING_AVERAGE] = [0f32; ACCEL_ROLLING_AVERAGE];
+    let mut baros: [f32; BARO_ROLLING_AVERAGE] = [0f32; BARO_ROLLING_AVERAGE];
     
-    let mut iter: usize = 0;
+    // max iters before all rolling averages are filled
+    let mut iter: usize = ACCEL_ROLLING_AVERAGE.max(BARO_ROLLING_AVERAGE);
+
+
+    // ====================================================================
+    // MAIN KALMAN LOOP
+    // ====================================================================
 
     loop {
         let dt = start.elapsed().as_secs_f32();
         start = Instant::now();
 
+        // ====================================================================
+        // READINGS AND AVERAGES
+        // ====================================================================
+
+        // update accel rolling average
         match imu.accel() {
             Some((_, _, mut z)) => {
-                //filter.predict(vel*dt + 0.5*z*dt*dt, 1f32);
+                //pos_filter.predict(vel*dt + 0.5*z*dt*dt, 1f32);
                 //vel += z*dt;
                 for i in 1..10 {
                     accels[i] = accels[i-1];
                 }
 
+                // TODO tweak values perhaps make these come from stdev?
+                // filter out values that are likely noise
                 if z < 0.05f32 && z > -0.05f32 {
                     z = 0f32;
                 }
@@ -99,6 +190,7 @@ fn main() {
             _ => {},
         }
 
+        // update baro rolling average
         match baro.get_alt() {
             Some(alt) => {
                 //filter.update(alt, 0.5f32);
@@ -113,15 +205,51 @@ fn main() {
             },
         };
 
+        // fill imu and baro windows before averaging
+        if iter > 0 {
+            iter -= 1;
+            continue;
+        }
+
+        // average accel readings
+        let mut acc = 0f32;
+        for i in 0..ACCEL_ROLLING_AVERAGE {
+            acc += accels[i]
+        }
+        acc /= ACCEL_ROLLING_AVERAGE as f32;
+
+        // average barometer readings
+        let mut baro_alt = 0f32;
+        for i in 0..BARO_ROLLING_AVERAGE {
+            baro_alt += baros[i]
+        }
+        baro_alt /= BARO_ROLLING_AVERAGE as f32;
+
+
+        // ====================================================================
+        // KALMAN UPDATES
+        // ====================================================================
+
+        // TODO track and combine velocity error
+        // dx = V_i * t + 1/2at^2
+        pos_filter.predict(vel*dt + 0.5*acc*(dt*dt), ACCEL_NOISE * ACCEL_WEIGHT);
+        vel += acc*dt*10f32;
+
+        // update from baro
+        pos_filter.update(baro_alt, BARO_NOISE * BARO_WEIGHT);
+
+        // update from GPS
         match gps.get_data() {
             Some(data) => {
+                // update altitude
                 match data.alt {
                     Some((alt, var)) => {
-                        filter.update(alt, var);
+                        pos_filter.update(alt, var);
                     },
                     _ => {},
                 };
 
+                // update tracked speed
                 match data.speed {
                     Some((speed, _)) => {
                         println!("gps_vel: {}", speed);
@@ -137,32 +265,9 @@ fn main() {
             _ => {},
         }
 
-        if iter < 10 {
-            iter += 1;
-            continue;
-        }
-
-        let mut total = 0f32;
-        for i in 0..10 {
-            total += accels[i]
-        }
-        total /= 10f32;
-
-        filter.predict(vel*dt*10f32 + 0.5*total*(10f32*dt*dt), 1f32);
-        vel += total*dt*10f32;
-
-        println!("acc: {}", total);
-
-        total = 0f32;
-        for i in 0..10 {
-            total += baros[i]
-        }
-        total /= 10f32;
-
-        filter.update(total, 0.5f32);
-        println!("baro: {}", total);
-
-        println!("alt: {:4.1}, var: {}, vel: {}, dt: {}", filter.alt, filter.var, vel, dt);
+        println!("baro: {}", baro_alt);
+        println!("acc: {}", acc);
+        println!("alt: {:4.1}, var: {}, vel: {}, dt: {}", pos_filter.alt, pos_filter.var, vel, dt);
     }
     
 
