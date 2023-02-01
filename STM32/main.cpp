c58075844145afe0adea3878ca89b29d72c29253
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2023-02-01 00:31:05 -0800

    merged baro and imu value displays includes mbed os build files

--- STM32/main.cpp
+++ STM32/main.cpp
@@ -1,6 +1,7 @@
 #include "mbed.h"
 #include "SerialStream.h"
 #include "IMU.h"
+#include "BARO.h"
 #include <cstdio>
 
 BufferedSerial serial(USBTX, USBRX, 115200);
@@ -9,6 +10,7 @@ SerialStream<BufferedSerial> pc(serial);
 
 I2C i2c(I2C_SDA, I2C_SCL);   // SDA, SCL
 IMU imu(i2c);  // Reset =D7, addr = BNO055_G_CHIP_ADDR, mode = MODE_NDOF <- as default
+Baro baro(&i2c);
 
 struct str {
     char c[12];
@@ -40,37 +42,51 @@ str to_str(double num) {
 int main()
 {
 
+    // ================== CONFIG IMU ==================
     pc.printf("getting IMU\n");
-
-    for (int i = 0; i<1000; i++) {
-        imu.accel();
-    }
-
+    
     calib state = imu.get_calibration();
     pc.printf("sys: %d, acc: %d, gyr: %d, mag: %d", state.sys, state.acc, state.gyr, state.mag);
 
     vec3 res = imu.calibrate_static_error(300);
-
     pc.printf("X: %s, Y: %s, Z: %s\n", to_str(res.x).c, to_str(res.y).c, to_str(res.z).c);
 
     double error = imu.get_noise(400);
     pc.printf("noise: %s\n", to_str(error).c);
 
-    ThisThread::sleep_for(10s);
-    
-    while(1)
-    {
+    // ==================
 
+    // ================== CONFIG BARO ==================
+
+    pc.printf("configuring baromter\n");
+    baro.configure(18.0, 150);
+
+    pc.printf("getting noise\n");
+    double noise = baro.get_noise(150);
+
+    pc.printf("noise: %s\n", to_str(noise).c);
+
+    // ==================
+    
+    while (true) {
+        auto at = baro.get_alt();
+        /*if (at.has) {
+            pc.printf("alt: %s pres: %d   temp: %s\n", to_str(at.val).c, baro.get_pressure().val, to_str(baro.get_temperature().val).c);
+        }*/
+        
+        
         vec3 angles = imu.euler();
         quat quat_d = imu.quaternion();
         vec3 accels = imu.accel();
 
-        pc.printf("H: %s, R: %s, P: %s  ||  %s %s %s %s  ||  X: %s, Y: %s, Z: %s\n",
+        pc.printf("H: %s, R: %s, P: %s  ||  %s %s %s %s  ||  X: %s, Y: %s, Z: %s  ||  %s\n",
             to_str(angles.x).c, to_str(angles.y).c, to_str(angles.z).c,
             to_str(quat_d.w).c, to_str(quat_d.x).c, to_str(quat_d.y).c, to_str(quat_d.z).c,
-            to_str(accels.x).c, to_str(accels.y).c, to_str(accels.z).c
+            to_str(accels.x).c, to_str(accels.y).c, to_str(accels.z).c,
+            to_str(at.val).c
         );
 
-        ThisThread::sleep_for(10ms);
+        //ThisThread::sleep_for(10ms);
     }
+
 }
