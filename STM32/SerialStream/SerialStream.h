8f360df1e7afbb7f66ad35c54005fff147bb92f7
Ethan Armstrong <w.ethan.armstrong@gmail.com>
2023-02-01 16:20:35 -0800

    moved double to str function to SerialStream class fixed to_str function to properly display decimals < 0.1

--- STM32/SerialStream/SerialStream.h
+++ STM32/SerialStream/SerialStream.h
@@ -1,7 +1,61 @@
 #ifndef SERIALSTREAM_H
 #define SERIALSTREAM_H
 
+#include "mbed.h"
 #include <platform/Stream.h>
+
+struct str {
+    char c[12];
+};
+
+inline str to_str(double num) {
+    str out;
+
+    int l = (int)num;
+    num -= l;
+
+    int r = abs(num*1000);
+
+    char sign[2] = {'+',0};
+    if (num < 0)
+        sign[0] = '-';
+
+    num = abs(num);
+
+    char zs[3] = {0, 0, 0};
+    
+    if (num < 0.1)
+        zs[0] = '0';
+    if (num < 0.01)
+        zs[1] = '0';
+
+    for (int i = 0; i<2; i++) {
+        num *= 10;
+        if (num >= 1) {
+            break;
+        }
+        zs[i] = '0';
+
+    }
+
+
+    sprintf(out.c, "%s%d.%s%3d", sign, l, zs, r);
+
+    bool found = false;
+    for (int i = 0; i<12; i++) {
+        if (out.c[i] == 0) {
+            found = true;
+        }
+        if (found) {
+            out.c[i] = ' ';
+        }
+    }
+
+    out.c[11] = 0;
+
+    return out;
+}
+
 
 /**
  * SerialStream
